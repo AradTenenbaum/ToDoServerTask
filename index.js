329161a97ce2d29d9a14ffdd9ab5bbd90d36bb6e
AradTenenbaum <arad.tenen@gmail.com>
2023-06-02 15:09:14 +0300

    add cors

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
 const express = require("express");
 const cors = require("cors");
+const { v4: uuidv4 } = require("uuid");
+const cookieParser = require("cookie-parser");
+var bodyParser = require("body-parser");
 
 const taskRoute = require("./routes/tasks");
 
 const app = express();
 
+app.use(
+  cors({
+    origin: `http://localhost:3000`,
+    credentials: true,
+    allowedHeaders: ["Content-Type"],
+    // optionsSuccessStatus: 200,
+  })
+);
 app.use(express.json());
-app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.send("hello world");
+});
+
+app.get("/user", (req, res) => {
+  const userId = req.cookies?.userId || uuidv4();
+  res.cookie("userId", userId).send({ id: userId });
 });
 
 app.use("/task", taskRoute);
--- services/task.js
+++ services/task.js
@@ -1,11 +1,21 @@
 let tasks = [];
 
 const getAllTasks = (req, res) => {
+  const userId = req.cookies?.userId;
+  if (!userId) {
+    res.status(403).end();
+    return;
+  }
   return res.send(tasks);
 };
 
 const addTask = (req, res) => {
   try {
+    const userId = req.cookies?.userId;
+    if (!userId) {
+      res.status(403).end();
+      return;
+    }
     if (!req.body.text || (req.body.text && req.body.text.length === 0)) {
       return res
         .status(400)
@@ -25,6 +35,11 @@ const addTask = (req, res) => {
 
 const setTaskCompletion = (req, res) => {
   try {
+    const userId = req.cookies?.userId;
+    if (!userId) {
+      res.status(403).end();
+      return;
+    }
     const id = req.params.id;
     if (id >= tasks.length)
       return res.status(400).send({ error: "id not exists" });
@@ -38,6 +53,11 @@ const setTaskCompletion = (req, res) => {
 
 const clearDoneTasks = (req, res) => {
   try {
+    const userId = req.cookies?.userId;
+    if (!userId) {
+      res.status(403).end();
+      return;
+    }
     tasks = tasks.filter((task) => !task.isDone);
     return res.send(tasks);
   } catch (error) {
